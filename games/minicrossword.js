669ae8dc3b50e71939aba409f3d145a344c92cc2
Jakob S <Jakob.Snorrason@student.uib.no>
2024-03-11 22:08:09 +0100

    ∞???

--- games/minicrossword.js
+++ games/minicrossword.js
@@ -1,10 +1,14 @@
 import Game from './game.js'
 
 const scoreFormatter = (score) => {
+  if (!isFinite(Number(score))) {
+    return '∞???'
+  }
+
   let year = Number(score) % 31536000
 
-  const time = new Date(Number(score) * 1000)
-  const years = time.getUTCFullYear() === 1970 ? '' : `${time.getUTCFullYear() - 1970}y`
+  const time = new Date(year * 1000)
+  const years = Number(score) / 31536000 >= 1 ? `${Math.floor(Number(score) / 31536000)}y` : ''
   const months = time.getUTCMonth() === 0 ? '' : `${time.getUTCMonth()}m`
   const days = time.getUTCDate() === 1 ? '' : `${time.getUTCDate()}d`
   const hours = time.getUTCHours() === 0 ? '' : `${time.getUTCHours()}h`
@@ -13,7 +17,7 @@ const scoreFormatter = (score) => {
 
   const slowpoke = years !== '' || months !== '' || days !== ''
 
-  return `${years}${slowpoke ? ', ' : ''}${months}${days}${hours}${minutes}${seconds}`
+  return `${years}${slowpoke ? ', ' : ''}${months}${days}${hours}${minutes}${seconds}.`
 }
 
 export const TheMini = new Game(
@@ -22,7 +26,7 @@ export const TheMini = new Game(
     const match = message.content.match(/https:\/\/www\.nytimes\.com\/.*\?d=([\d-]+)&t=(\d+)/)
     return match ? { day: match[1], score: match[2] } : {}
   },
-  (e) => `${e.user.server_name} did the Mini Crossword ${e.day} in ${scoreFormatter(e.score)}.`,
+  (e) => `${e.user.server_name} did the Mini Crossword ${e.day} in ${scoreFormatter(e.score)}`,
   scoreFormatter,
   undefined,
   { inline: true, order: 1 },
