54345429f4f9e8459ac6bb35d49a1942b7bc954f
Peter <peter>
2025-05-10 10:14:10 +0200

    feat(mock): add in memory db for dev, better mock data generation, empty test and webstorm config

--- src/core/database/util.ts
+++ src/core/database/util.ts
@@ -1,12 +1,23 @@
 import mongoose from 'mongoose';
 import Config from '../../config.js';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 /**
  * Initializes a database connection or dies trying.
  */
 export async function init_database() {
+  let db_uri
+  const enable_dev_features = process.argv.includes('--dev')
+  if (enable_dev_features) {
+    const mongod = await MongoMemoryServer.create();
+    db_uri =  mongod.getUri();
+    console.info('\x1b[36m%s\x1b[0m', '--dev: Using in-memory database (uri: ' + db_uri)
+  } else {
+    db_uri = Config.DATABASE_URI
+  }
+
   await mongoose
-    .connect(Config.DATABASE_URI)
+    .connect(db_uri)
     .then(() => console.info('Connected to MongoDB.'))
     .catch((err) => {
       console.error(`Failed to connect to MongoDB: ${err}`);
---
+++ src/test/ut.test.ts
@@ -0,0 +1,7 @@
+import { describe, test, expect, jest } from '@jest/globals';
+
+describe('test', () => {
+    test('test', async () => {
+        expect(null).toBeNull();
+    });
+});
